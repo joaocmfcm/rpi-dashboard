0d2b6c045eddfd5c2b39e4c53e9d65069f202af3
João Martins <joaocmfcm@gmail.com>
2017-03-15 00:17:03 +0000

    Revert "Revert the revert"
    This reverts commit e711a7b0c473fc721bb69341227b315417bcd129.

---
+++ web/app/scripts/constants.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @description
+ * Static constants used accross the app
+ * @observations
+ * 
+ */
+
+angular.module('dashboardApp')
+	.constant('networkConstants', {apiUrl: 'http://localhost:3000/api', requestTimeout: 30000});
